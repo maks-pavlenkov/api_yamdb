a99b3f938fd13934670ec5a783bf979573631da0
Екатерина Яковлева <regspamm@yandex.ru>
2022-11-22 23:32:21 +0300

    Добавлено приложение users

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    USER = 'Пользователь'
+    MODERATOR = 'Модератор'
+    ADMIN = 'Администратор'
+    ROLES = [
+        (USER, "Пользователь"),
+        (MODERATOR, "Модератор"),
+        (ADMIN, "Администратор")
+    ]
+    role = models.CharField(
+        max_length=50,
+        choices=ROLES,
+        default=USER
+    )
