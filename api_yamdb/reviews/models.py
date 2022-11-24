efbfa6d97fb8bed3b2ddf36a5f3dbed900acd2d6
Екатерина Яковлева <regspamm@yandex.ru>
2022-11-24 16:19:44 +0300

    Fields added in User model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .validators import validate_username
+
 
 class User(AbstractUser):
     USER = "Пользователь"
@@ -11,8 +13,28 @@ class User(AbstractUser):
         (MODERATOR, "Модератор"),
         (ADMIN, "Администратор")
     ]
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        validators=[validate_username]
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True)
+    first_name = models.CharField(
+        max_length=150,
+        blank=True,
+        null=True)
+    last_name = models.CharField(
+        max_length=150,
+        blank=True,
+        null=True)
+    bio = models.TextField(
+        blank=True,
+        null=True)
     role = models.CharField(
         max_length=50,
         choices=ROLES,
         default=USER
     )
+
