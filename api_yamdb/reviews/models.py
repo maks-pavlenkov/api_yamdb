ff015313b6e2c532793afc716eb8177b2591430e
Maks Pavlenkov <coolpavlenkov@mail.ru>
2022-11-27 16:08:14 +0300

    Added scoring

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 from .validators import validate_username
 
@@ -74,10 +75,13 @@ class Title(models.Model):
 
 class Reviews(models.Model):
     text = models.TextField()
-    author = models.CharField(max_length=20)
+    author = models.CharField(max_length=20) # Will fix it later
     pub_date = models.DateTimeField(
         'Дата публикации', auto_now_add=True
     )
     title = models.ForeignKey(
         Title, on_delete=models.CASCADE, related_name='reviews'
+    )
+    score = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
     )
