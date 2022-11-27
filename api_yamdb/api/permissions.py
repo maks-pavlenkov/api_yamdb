619c8bbf72fd8a2a1e2d4318a66b7c3aa31f1a9f
Maks Pavlenkov <coolpavlenkov@mail.ru>
2022-11-27 22:02:14 +0300

    permission fix

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -19,5 +19,5 @@ class AuthorAdminOrReadOnly(BasePermission):
         return (
             obj.author == request.user
             or request.user.is_moderator
-            or request.is_admin_or_superuser
+            or request.user.is_admin_or_superuser
         )
