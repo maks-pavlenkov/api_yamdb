ddd2bed05552ec83e8afa9d52f768d9460ffb22c
Аня Головнина <don.anka88@gmail.com>
2022-12-02 16:59:59 +0300

    review fixing

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,20 +3,30 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsAdminOrSuperuser(BasePermission):
     def has_permission(self, request, view):
-        return (request.user.is_authenticated
-                and request.user.is_admin_or_superuser)
+        return (
+            request.user.is_authenticated
+            and request.user.is_admin_or_superuser
+        )
 
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or (request.user.is_authenticated
-                    and request.user.is_admin_or_superuser))
+        return (
+            request.method in SAFE_METHODS
+            or (
+                request.user.is_authenticated
+                and request.user.is_admin_or_superuser
+            )
+        )
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS
-                or (request.user.is_authenticated
-                    and request.user.is_admin_or_superuser))
+        return (
+            request.method in SAFE_METHODS
+            or (
+                request.user.is_authenticated
+                and request.user.is_admin_or_superuser
+            )
+        )
 
 
 class ReadOnly(BasePermission):
@@ -28,8 +38,10 @@ class ReadOnly(BasePermission):
 class AuthorAdminModeratorOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or request.user.is_authenticated)
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
         return (
